73aded84cd398b72ca797548b840457ae766edcb
actokuyt <chibthankgod@gmail.com>
2025-02-25 17:03:12 +0100

    cors fix 2

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://beatify-one.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://beatify-one.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
