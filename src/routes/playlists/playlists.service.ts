31586a94d5cda0c36488fe959888c16869180ed6
actokuyt <chibthankgod@gmail.com>
2025-05-04 19:41:43 +0100

    feat(playlists): refactor playlists service to include track details in findAll method

--- src/routes/playlists/playlists.service.ts
+++ src/routes/playlists/playlists.service.ts
@@ -1,15 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePlaylistDto } from './dto/create-playlist.dto';
 import { UpdatePlaylistDto } from './dto/update-playlist.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PlaylistsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createPlaylistDto: CreatePlaylistDto) {
     return 'This action adds a new playlist';
   }
 
-  findAll() {
-    return `This action returns all playlists`;
+  async findAll() {
+    const playlists = await this.prisma.playlist.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+      include: {
+        tracks: true,
+      },
+    });
+
+    const trackIds = playlists.flatMap((playlist) =>
+      playlist.tracks.map((playlistTrack) => playlistTrack.trackId),
+    );
+
+    const tracks = await this.prisma.track.findMany({
+      where: {
+        id: {
+          in: trackIds,
+        },
+      },
+    });
+
+    const playlistsWithTracks = playlists.map((playlist) => ({
+      ...playlist,
+      tracks: playlist.tracks.map((playlistTrack) =>
+        tracks.find((track) => track.id === playlistTrack.trackId),
+      ),
+    }));
+
+    return playlistsWithTracks;
   }
 
   findOne(id: number) {
