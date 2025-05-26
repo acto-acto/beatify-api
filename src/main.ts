083c1eadb390814e4fe48e65461dee640d2d471b
actokuyt <chibthankgod@gmail.com>
2025-05-26 15:23:32 +0100

    feat(cors): refactor CORS configuration to use a whitelist for origin validation

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+const whitelist = [
+  'http://localhost:3000',
+  'https://beatify-one.vercel.app',
+  'https://beatify-5c2qk3r6d-actokuyts-projects.vercel.app/',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://beatify-one.vercel.app',
-      'https://vercel.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    origin: (origin, callback) => {
+      if (!origin || whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-requires-auth'],
   });
   app.use(cookieParser());
