37689dac0eb24b4a18bb2f31d3a22f19d1942c92
actokuyt <chibthankgod@gmail.com>
2025-05-26 15:24:14 +0100

    feat(cors): simplify CORS configuration by using a static origin array

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-const whitelist = [
-  'http://localhost:3000',
-  'https://beatify-one.vercel.app',
-  'https://beatify-5c2qk3r6d-actokuyts-projects.vercel.app/',
-];
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || whitelist.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
+    origin: [
+      'http://localhost:3000',
+      'https://beatify-one.vercel.app',
+      'https://beatify-5c2qk3r6d-actokuyts-projects.vercel.app/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-requires-auth'],
   });
   app.use(cookieParser());
