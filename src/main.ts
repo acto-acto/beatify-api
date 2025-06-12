379261fcf333be8c42a8f337f388d4dc1a11dbc8
actokuyt <chibthankgod@gmail.com>
2025-06-12 11:53:16 +0100

    feat(cors): remove deprecated origin from CORS configuration for cleaner setup

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://beatify-one.vercel.app',
-      'https://beatify-5c2qk3r6d-actokuyts-projects.vercel.app/',
-    ],
+    origin: ['http://localhost:3000', 'https://beatify-one.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-requires-auth'],
