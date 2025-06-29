8eb4de8f04ad1e13ca6a20efa1b18b7ecd34fd70
actokuyt <chibthankgod@gmail.com>
2025-06-29 07:55:12 +0100

    feat(cors): add additional origin to CORS configuration for improved access

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://beatify-one.vercel.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://beatify-one.vercel.app',
+      'https://beatify.actotech.dev/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-requires-auth'],
